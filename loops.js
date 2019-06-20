e64d9dda3fcbea006d1acbe86c58cd74d0b029f8
Karina Evers <krin_11_bball@yahoo.com>
2019-06-20 21:38:33 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,15 +1,12 @@
-function forLoop(array) {
-  for (let i = 0; i < 25; i++) {
+function forLoop(array){
+  for (var i = 0; i < 25; i++) {
     if (i === 1) {
       array.push(`"I am ${i} strange loop"`)
     } else {
       array.push(`"I am ${i} strange loops"`)
-      }
     }
+  }
     return array
 }
 
 
-function whileLoop(10) {
-
-}
